ab954e5ab8c00a9455818fab3cc948b899c9723a
Hiromu Nakano <hiromu.nakano.12@gmail.com>
2020-04-09 20:31:57 +0900

    Initial Commit

---
+++ GoodMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GoodMe
+//
+//  Created by Hiromu Nakano on 2020/04/09.
+//  Copyright © 2020 Hiromu Nakano. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
